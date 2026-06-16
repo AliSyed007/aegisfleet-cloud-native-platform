d9d61de6989070dba3531fc6fdf71b51e1284cb5
Sayed Mahmeer Ali Shah <ali147759@gmail.com>
2026-06-16 17:16:27 +0500

    Add telemetry event insert helper

--- api/database.py
+++ api/database.py
@@ -30,3 +30,38 @@ def get_database_config():
 
 def get_database_connection():
     return psycopg.connect(**get_database_config())
+
+
+def save_telemetry_event(telemetry_data):
+    insert_query = """
+        INSERT INTO telemetry_events (
+            vehicle_id,
+            lat,
+            lon,
+            battery_level,
+            temperature,
+            speed_kmh,
+            health_status,
+            alert,
+            received_at
+        )
+        VALUES (
+            %(vehicle_id)s,
+            %(lat)s,
+            %(lon)s,
+            %(battery_level)s,
+            %(temperature)s,
+            %(speed_kmh)s,
+            %(health_status)s,
+            %(alert)s,
+            %(received_at)s
+        )
+        RETURNING id;
+    """
+
+    with get_database_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(insert_query, telemetry_data)
+            telemetry_event_id = cur.fetchone()[0]
+
+    return telemetry_event_id
